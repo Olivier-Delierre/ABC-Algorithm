761e6176549fb62e23ec285af83a775b998696ec
Olivier Delierre <olivier.delierre@gmail.com>
2017-10-19 09:07:24 +0200

    Ajout des classes imposées

---
+++ include/SetUpParams.h
@@ -0,0 +1,36 @@
+#ifndef SETUPPARAMS_H
+#define SETUPPARAMS_H
+
+#include <iostream>
+
+using namespace std;
+
+class SetUpParams
+{
+	//ici vous devez mettre quelques paramètres tels que :
+
+private:
+	unsigned int   _independent_runs;         //number of independent runs
+	unsigned int   _nb_evolution_steps;       // number of iterations per run
+	unsigned int   _population_size;		// number of solutions in the population
+	unsigned int   _solution_size;	        // size of each particle
+
+public:
+	SetUpParams();
+
+	friend ostream& operator<< (ostream& os, const SetUpParams& setup);
+	friend istream& operator>> (istream& is, SetUpParams& setup);
+
+	const unsigned int   independent_runs() const;
+	const unsigned int   nb_evolution_steps() const;
+	const unsigned int   population_size() const;
+	const unsigned int   solution_size() const;
+	void independent_runs(const unsigned int val);
+	void nb_evolution_steps(const unsigned int val);
+	void population_size(const unsigned int val);
+	void solution_size(const unsigned int val);
+
+	~SetUpParams();
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-
-using namespace std;
+#include "../include/MyAlgorithm.h"
 
 int main()
 {
