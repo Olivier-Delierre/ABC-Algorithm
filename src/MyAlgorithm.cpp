c5848dbabb65728b510e60b860708b31e309b149
antoninleclerc <antonin.leclerc@orange.fr>
2017-11-30 09:53:22 +0100

    Destructeur de MyAlgorithm, fonction upper_cost et lower_cost

--- src/MyAlgorithm.cpp
+++ src/MyAlgorithm.cpp
@@ -8,3 +8,35 @@ MyAlgorithm::MyAlgorithm(const Problem& pbm, const SetUpParams& setup) :
 	for (int i = 0; i < _setup.population_size(); i++)
 		_solutions[i] = new Solution{ pbm };
 }
+
+/**
+Destructor of MyAlgorithm
+
+*/
+MyAlgorithm::~MyAlgorithm()
+{
+	for (int i = 0; i < _solutions.size(); i++)
+	{
+		delete _solutions[i];
+	}
+}
+
+/**
+Return the upper fitness of individuals in population
+
+@return _upper_cost - returns the upper fitness of individuals in population
+*/
+unsigned int MyAlgorithm::upper_cost() const
+{
+	return _upper_cost;
+}
+
+/**
+Return the lower fitness of individuals in population
+
+@return _lower_cost - returns the lower fitness of individuals in population
+*/
+unsigned int MyAlgorithm::lower_cost() const
+{
+	return _lower_cost;
+}
