35b16d5d16f4e4310455f0be14bfcd137ccf294b
MichaelGerwill <michael.gerwill@gmail.com>
2017-12-05 17:45:19 +0100

    Ajout du choix du bench

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -70,9 +70,49 @@ double Solution::get_fitness()
 	return min;
 }
 
+void Solution::choixBenchmark()
+{
+	std::cout << "Choisissez le benchmark: (entrez un entier)" << std::endl;
+	std::cout << "1- ROSENBROCK" << std::endl;
+	std::cout << "2- RASTRIGIN" << std::endl;
+	std::cout << "3- ACKLEY" << std::endl;
+	std::cout << "4- SCHWEFEL" << std::endl;
+	std::cout << "5- SCHAFFER" << std::endl;
+	std::cout << "6- WEIERSTRASS" << std::endl;
+	int choix;
+	std::cin >> choix;
+	while (choix != 1 && choix != 2 && choix != 3 && choix != 4 && choix != 5 && choix != 6)
+	{
+		std::cout << "Veuillez saisir un entier compris entre 1 et 6." << std::endl;
+		std::cout << std::endl;
+		std::cout << "Choisissez le benchmark: (entrez un entier)" << std::endl;
+		std::cout << "1- ROSENBROCK" << std::endl;
+		std::cout << "2- RASTRIGIN" << std::endl;
+		std::cout << "3- ACKLEY" << std::endl;
+		std::cout << "4- SCHWEFEL" << std::endl;
+		std::cout << "5- SCHAFFER" << std::endl;
+		std::cout << "6- WEIERSTRASS" << std::endl;
+	}
+	_choice = choix;
+}
+
 double Solution::f() const
 {
-	return 0;
+	switch (_choice)
+	{
+	case ROSENBROCK:
+		return Rosenbrock();
+	case RASTRIGIN:
+		return Rastrigin();
+	case ACKLEY:
+		return Ackley();
+	case SCHWEFEL:
+		return Schwefel();
+	case SCHAFFER:
+		return Schaffer();
+	case WEIERSTRASS:
+		return Weierstrass();
+	}
 }
 
 double Solution::Rosenbrock()	const
